806f35953cba0bd2adecddf922a5058929ae3459
Kauan Lopes <98911353+OnlyKau@users.noreply.github.com>
2023-10-09 14:49:04 -0300

    Update bitfield.go

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -1,9 +1,9 @@
 package bitfield
 
-// A Bitfield represents the pieces that a peer has
+
 type Bitfield []byte
 
-// HasPiece tells if a bitfield has a particular index set
+
 func (bf Bitfield) HasPiece(index int) bool {
 	byteIndex := index / 8
 	offset := index % 8
@@ -13,12 +13,12 @@ func (bf Bitfield) HasPiece(index int) bool {
 	return bf[byteIndex]>>uint(7-offset)&1 != 0
 }
 
-// SetPiece sets a bit in the bitfield
+
 func (bf Bitfield) SetPiece(index int) {
 	byteIndex := index / 8
 	offset := index % 8
 
-	// silently discard invalid bounded index
+	
 	if byteIndex < 0 || byteIndex >= len(bf) {
 		return
 	}
