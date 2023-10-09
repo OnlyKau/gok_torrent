92cf9d2ec81f4a8b6aaddaff5ce2308d4fa1709a
Kauan Lopes <kauan.iwr@gmail.com>
2023-10-08 22:19:38 -0300

    Almost There

--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module gok_torrent
 go 1.21.2
 
 require github.com/jackpal/bencode-go v1.0.0 // indirect
+
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"gok_torrent/torrentfile"
+	"log"
+	"os"
+)
+
+func main() {
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrentfile.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
